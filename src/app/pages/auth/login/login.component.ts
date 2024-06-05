fc5d29e9d0dde38ef3fcb1eaaa95cfa2491a103e
BanSolo03 <nikibano@gmail.com>
2024-06-05 11:43:44 +0200

    added login Validators

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -8,18 +8,56 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() {}
+  constructor() {
+    
+  }
 
-  loginForm!: FormGroup
+  loginForm!: FormGroup;
+  errors: string[] = [];
 
   ngOnInit(): void {
     this.loginForm = new FormGroup({
-      email: new FormControl(Validators.required, Validators.email),
+      email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl()
+    });
+
+    this.loginForm.valueChanges.subscribe((res: any) => {
+      this.getErrors();
     })
   }
 
   login(): void {
-    
+    if(this.loginForm.valid) {
+
+    } else {
+      this.getErrors();
+    }
+  }
+
+  getErrors() {
+    this.errors = [];
+    if (this.loginForm.invalid) {
+      Object.keys(this.loginForm.controls).forEach(field => {
+        const control = this.loginForm.get(field);
+        if (control && control.errors) {
+          Object.keys(control.errors).forEach(errorKey => {
+            let errorMessage = '';
+            switch (errorKey) {
+              case 'required':
+                errorMessage = `${field} è richiesto.`;
+                break;
+              case 'email':
+                errorMessage = `Email non valida`;
+                break;
+                // Aggiungi qui altri casi per gestire altri tipi di errori
+              default:
+                errorMessage = `${field}: errore non specificato.`;
+                break;
+            }
+            this.errors.push(errorMessage);
+          });
+        }
+      });
+    }
   }
 }
