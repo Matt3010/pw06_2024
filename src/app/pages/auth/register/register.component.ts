93f4341ec675ae62abcbaea52410db578b8044c6
Matteo Scanferla <m.scanferla@eggonemail.com>
2024-06-05 11:25:28 +0200

    added registration form validations

--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {Component, EventEmitter, Output} from '@angular/core';
+import {FormControl, FormGroup, Validator, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -8,13 +8,62 @@ import {FormControl, FormGroup} from "@angular/forms";
 })
 export class RegisterComponent {
 
+  errors: string[] = [];
+
   registerForm: FormGroup = new FormGroup({
-    email: new FormControl(),
-    passowrd: new FormControl(),
-    confirm_password: new FormControl(),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+    confirm_password: new  FormControl('', [Validators.required]),
   })
 
   constructor() {
+    this.registerForm.valueChanges.subscribe((res: any) => {
+        this.getErrors();
+    })
   }
+
+
+  onSubmit() {
+    if(this.registerForm.valid) {
+
+    } else {
+      this.getErrors();
+    }
+  }
+
+
+  getErrors() {
+    this.errors = [];
+    if (this.registerForm.invalid) {
+      Object.keys(this.registerForm.controls).forEach(field => {
+        const control = this.registerForm.get(field);
+        if (control && control.errors) {
+          Object.keys(control.errors).forEach(errorKey => {
+            let errorMessage = '';
+            switch (errorKey) {
+              case 'required':
+                errorMessage = `${field} è richiesto.`;
+                break;
+              case 'minlength':
+                errorMessage = `${field} deve avere almeno ${control.errors![errorKey].requiredLength} caratteri.`;
+                break;
+              case 'maxlength':
+                errorMessage = `${field} non può superare ${control.errors![errorKey].requiredLength} caratteri.`;
+                break;
+              case 'email':
+                errorMessage = `Email non valida`;
+                break;
+                // Aggiungi qui altri casi per gestire altri tipi di errori
+              default:
+                errorMessage = `${field}: errore non specificato.`;
+                break;
+            }
+            this.errors.push(errorMessage);
+          });
+        }
+      });
+    }
+  }
+
 
 }
