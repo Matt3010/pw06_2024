8448228277a89e155aca081d7e371a8ed0066c22
Matteo Scanferla <m.scanferla@eggonemail.com>
2024-06-18 10:35:38 +0200

    order items details

--- src/app/pages/orders/orders.component.ts
+++ src/app/pages/orders/orders.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {OrderService} from "../../_services/order.service";
-import {TreeNode} from "primeng/api";
+import {ActivatedRoute, Params, Router} from "@angular/router";
+import {Observable} from "rxjs";
 import {Order} from "../../../@data/order";
 
 @Component({
@@ -11,11 +12,25 @@ import {Order} from "../../../@data/order";
 export class OrdersComponent {
 
     orders$ = this.orderService.orders$;
+    queryParams$: Observable<Params>;
+    item: Order | null = null;
 
     constructor(
-        public orderService: OrderService
+        public orderService: OrderService,
+        public activatedRouter: ActivatedRoute,
+        private router: Router
     ) {
+        this.queryParams$ = this.activatedRouter.queryParams;
     }
 
+    navigate(item: any) {
+        this.item = item;
+        this.router.navigateByUrl('/pages/dashboard?id=' + item._id);
+    }
+
+    closeItemDetails() {
+        this.item = null;
+        this.router.navigateByUrl('/pages/dashboard');
+    }
 
 }
