319ae5e497f12315458a50940dc8543580fe16d6
Matteo Scanferla <m.scanferla@eggonemail.com>
2024-06-21 12:29:01 +0200

    chenaged color dashboard

--- src/app/_services/afk.service.ts
+++ src/app/_services/afk.service.ts
@@ -1,42 +1,43 @@
-import { Injectable, NgZone } from '@angular/core';
-import { Router } from '@angular/router';
-import { TokenService } from './token.service';
-import { AuthService } from './auth.service';
+import {Injectable, NgZone} from '@angular/core';
+import {AuthService} from './auth.service';
+import {ComponentInjectorService} from "../_utils/component-injector.service";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AfkService {
-  private timeoutId: any;
-  private readonly timeoutDuration: number = 15 * 60 * 1000;
+    private timeoutId: any;
+    private readonly timeoutDuration: number = 15 * 60 * 1000;
 
-  constructor(
-    private ngZone: NgZone,
-    private authService: AuthService
-  ) {
-    this.startTimer();
-    this.initializeEventListeners();
-  }
+    constructor(
+        private ngZone: NgZone,
+        private authService: AuthService,
+        private injectorService: ComponentInjectorService
+    ) {
+        this.startTimer();
+        this.initializeEventListeners();
+    }
 
-  private initializeEventListeners(): void {
-    ['click', 'mousemove', 'keypress', 'touchstart'].forEach(event => 
-      window.addEventListener(event, () => this.resetTimer()));
-  }
+    private initializeEventListeners(): void {
+        ['click', 'mousemove', 'keypress', 'touchstart'].forEach(event =>
+            window.addEventListener(event, () => this.resetTimer()));
+    }
 
-  private startTimer(): void {
-    this.ngZone.runOutsideAngular(() => {
-      this.timeoutId = setTimeout(() => this.logout(), this.timeoutDuration);
-    });
-  }
+    private startTimer(): void {
+        this.ngZone.runOutsideAngular(() => {
+            this.timeoutId = setTimeout(() => this.logout(), this.timeoutDuration);
+        });
+    }
 
-  private resetTimer(): void {
-    clearTimeout(this.timeoutId);
-    this.startTimer();
-  }
+    private resetTimer(): void {
+        clearTimeout(this.timeoutId);
+        this.startTimer();
+    }
 
-  private logout(): void {
-    this.ngZone.run(() => {
-      this.authService.logout();
-    });
-  }
+    private logout(): void {
+        this.ngZone.run(() => {
+            this.injectorService.destroyComponent();
+            this.authService.logout();
+        });
+    }
 }
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,50 +1,48 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {pages} from "./pages";
 import {ComponentInjectorService} from "../../_utils/component-injector.service";
-import {MdSmComponent} from "../../components/modals-templates/md-sm/md-sm.component";
 import {MdXlComponent} from "../../components/modals-templates/md-xl/md-xl.component";
 import {Router} from "@angular/router";
 import {MdMdComponent} from "../../components/modals-templates/md-md/md-md.component";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
 
-  blocks = pages;
+    blocks = pages;
 
-  constructor(
-      private injectorService: ComponentInjectorService,
-      private router: Router,
-  ) {
-  }
-
-
-  stringToColor(string: any, saturation = 40, lightness = 90) {
-    let hash = 0;
-    for (let i = 0; i < string.length; i++) {
-      hash = string.charCodeAt(i) + ((hash << 5) - hash);
-      hash = hash & hash;
+    constructor(
+        private injectorService: ComponentInjectorService,
+        private router: Router,
+    ) {
     }
-    return `hsl(${(hash % 345)}, ${saturation}%, ${lightness}%)`;
-  }
 
 
-  openModal(component: any, title: string, url: string | null, size: string) {
-    if(!size) {
-      this.injectorService.createComponent(MdXlComponent, {component: component, title: title, goTo: url});
-    } else if (size === 'md') {
-        this.injectorService.createComponent(MdMdComponent, {component: component, title: title, goTo: url});
+    stringToColor(string: any, saturation = 65, lightness = 90) {
+        let hash = 0;
+        for (let i = 0; i < string.length; i++) {
+            hash = string.charCodeAt(i) + ((hash << 5) - hash);
+            hash = hash & hash;
+        }
+        return `hsl(${(hash % 345)}, ${saturation}%, ${lightness}%)`;
     }
-  }
 
-  openPreview(url: string, event: MouseEvent) {
-    this.router.navigateByUrl(url);
-    event.preventDefault();
-  }
 
+    openModal(component: any, title: string, url: string | null, size: string) {
+        if (!size) {
+            this.injectorService.createComponent(MdXlComponent, {component: component, title: title, goTo: url});
+        } else if (size === 'md') {
+            this.injectorService.createComponent(MdMdComponent, {component: component, title: title, goTo: url});
+        }
+    }
+
+    openPreview(url: string, event: MouseEvent) {
+        this.router.navigateByUrl(url);
+        event.preventDefault();
+    }
 
 
 }
